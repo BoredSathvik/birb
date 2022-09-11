adcac451aeb95aa1266b4a953382adc97c08d969
boredsathvik <sathvikr13@gmail.com>
2022-09-11 07:26:50 +0530

    Not useful commit

--- client/src/gui/gui.cpp
+++ client/src/gui/gui.cpp
@@ -1,8 +1,3 @@
-// Primary color #EF6C00
-// Danger color #dc3545
-// Success color #28A745
-// Warning color #FFC107
-
 #include "gui.h"
 
 Gui::Gui() { this->dx_manager = new DxManager(); }
@@ -18,8 +13,6 @@ void Gui::Init() {
     ImGui_ImplWin32_Init(window);
     ImGui_ImplDX11_Init(dx_manager->d3d_device.get(), dx_manager->d3d_context.get());
 
-    // dx_manager->d2d_context.get()->CreateSolidColorBrush(D2D1::ColorF(1.0f, 1.0f, 1.0f, 1.0f), rect_brush.put());
-
     Logger::LogF("Gui initialized");
 }
 void Gui::CleanGui() {
@@ -27,7 +20,6 @@ void Gui::CleanGui() {
     ImGui_ImplWin32_Shutdown();
     ImGui::DestroyContext();
 
-    // this->rect_brush = nullptr;
     delete this->dx_manager;
 }
 
@@ -144,12 +136,6 @@ void Gui::Render() {
     Global::width = dx_manager->window_size[0];
     Global::height = dx_manager->window_size[1];
 
-    // ImGui::ShowDemoWindow();
-    // ImGui::ShowStyleEditor();
-    // ImGui::ShowFontAtlas(ImGui::GetIO().Fonts);
-
-    // if (*this->show_gui && this->show) {
-
     if (this->show) {
         bool open = true;
         ImGui::SetNextWindowPos(ImVec2(x, y));
@@ -234,7 +220,6 @@ void Gui::Render() {
         // End of Content Window
 
         ImGui::End();
-        // } else if (*this->show_hud) {
     } else if (true) {
         for (auto module : *ModuleManager::GetInstance()->modules) {
             module->OnHUDRender();
@@ -247,60 +232,5 @@ void Gui::Render() {
     ID3D11RenderTargetView* targets[1] = {dx_manager->d3d_render_target_view.get()};
     dx_manager->d3d_context->OMSetRenderTargets(1, targets, nullptr);
     dx_manager->d3d_context->ClearRenderTargetView(dx_manager->d3d_render_target_view.get(), color);
-    // dx_manager->d3d_context->ClearDepthStencilView(dx_manager->d3d_depth_stencil_view.get(), D3D11_CLEAR_DEPTH | D3D11_CLEAR_STENCIL, 1.0f, 0);
     ImGui_ImplDX11_RenderDrawData(ImGui::GetDrawData());
-}
-void Gui::RenderBackground() {
-    // struct Vertex {
-    //     float x;
-    //     float y;
-    // };
-
-    // const Vertex vertices[] = {{0.0f, 0.5f}, {0.5f, -0.5f}, {-0.5, -0.5f}};
-
-    // winrt::com_ptr<ID3D11Buffer> vertex_buffer;
-    // D3D11_BUFFER_DESC bd = {};
-    // bd.BindFlags = D3D11_BIND_VERTEX_BUFFER;
-    // bd.Usage = D3D11_USAGE_DEFAULT;
-    // bd.CPUAccessFlags = 0;
-    // bd.MiscFlags = 0;
-    // bd.ByteWidth = sizeof(vertices);
-    // bd.StructureByteStride = sizeof(Vertex);
-
-    // D3D11_SUBRESOURCE_DATA sd = {};
-    // sd.pSysMem = vertices;
-
-    // dx_manager->d3d_device->CreateBuffer(&bd, &sd, vertex_buffer.put());
-
-    // // Binds vertex buffer to pipeline
-    // u_int stride = sizeof(Vertex);
-    // u_int offset = 0;
-    // dx_manager->d3d_context->IASetVertexBuffers(0, 1, vertex_buffer.put(), &stride, &offset);
-
-    // // vertex shader
-    // winrt::com_ptr<ID3DBlob> blob;
-    // winrt::com_ptr<ID3D11PixelShader> pixel_shader;
-    // D3DReadFileToBlob(L"C:/Users/sathv/AppData/Local/Packages/Microsoft.MinecraftUWP_8wekyb3d8bbwe/LocalCache/birb/shaders/ps.cso", blob.put());
-    // dx_manager->d3d_device->CreatePixelShader(blob->GetBufferPointer(), blob->GetBufferSize(), nullptr, pixel_shader.put());
-    // dx_manager->d3d_context->PSSetShader(pixel_shader.get(), nullptr, 0);
-
-    // // vertex shader
-    // winrt::com_ptr<ID3D11VertexShader> vertex_shader;
-    // D3DReadFileToBlob(L"C:/Users/sathv/AppData/Local/Packages/Microsoft.MinecraftUWP_8wekyb3d8bbwe/LocalCache/birb/shaders/vs.cso", blob.put());
-    // dx_manager->d3d_device->CreateVertexShader(blob->GetBufferPointer(), blob->GetBufferSize(), nullptr, vertex_shader.put());
-    // dx_manager->d3d_context->VSSetShader(vertex_shader.get(), nullptr, 0);
-
-    // // Creates input layout for the vertex shader
-    // winrt::com_ptr<ID3D11InputLayout> input_layout;
-    // const D3D11_INPUT_ELEMENT_DESC ied[] = {{"Position", 0, DXGI_FORMAT_R32G32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0}};
-    // dx_manager->d3d_device->CreateInputLayout(ied, (u_int)std::size(ied), blob->GetBufferPointer(), blob->GetBufferSize(), input_layout.put());
-    // dx_manager->d3d_context->IASetInputLayout(input_layout.get());
-
-    // // sets topology
-    // dx_manager->d3d_context->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
-
-    // // sets rendertarget to rendertarget ez
-
-    // // draws everything
-    // dx_manager->d3d_context->Draw((u_int)std::size(vertices), 0);
 }
